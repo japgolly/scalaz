7d52856b448eb92a8361e3d683d86999f1b0f8f4
David Barri <japgolly@gmail.com>
2014-07-10 21:24:41 +1000

    Releasing to maven central instead of bintray

--- README.md
+++ README.md
@@ -1,20 +1,10 @@
 # Scalaz on Scala.JS
 
-This has been compiled for Scala.JS and published to bintray under `japgolly.scalaz`.
+This has been compiled for Scala.JS and published to Maven central under `com.github.japgolly.fork.scalaz`.
 
 #### Usage
 
-project/plugins.sbt
-```
-addSbtPlugin("me.lessis" % "bintray-sbt" % "0.1.1")
-```
-
 build.sbt
 ```
-resolvers += bintray.Opts.resolver.repo("japgolly", "forks")
-
-libraryDependencies += "japgolly.scalaz" %%% "scalaz-core" % "7.1.0-RC1"
+libraryDependencies += "com.github.japgolly.fork.scalaz" %%% "scalaz-core" % "7.1.0-RC1"
 ```
-
-#### Manual Download
-https://bintray.com/japgolly/forks/japgolly.scalaz
--- project/build.scala
+++ project/build.scala
@@ -45,15 +45,10 @@ object build extends Build {
 
   def scalaCheckVersion = "1.11.4"
 
-  import bintray.{Keys => BK}
   lazy val standardSettings: Seq[Sett] = Defaults.defaultSettings ++ sbtrelease.ReleasePlugin.releaseSettings ++
                                          scala.scalajs.sbtplugin.ScalaJSPlugin.scalaJSBuildSettings ++
-                                         bintray.Plugin.bintrayPublishSettings ++
                                          Seq[Sett](
-    licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
-    organization := "japgolly.scalaz",
-    BK.repository in BK.bintray := "forks",
-    publishMavenStyle := true,
+    organization := "com.github.japgolly.fork.scalaz",
 
     scalaVersion := "2.10.4",
     crossScalaVersions := Seq("2.9.3", "2.10.4", "2.11.1"),
@@ -110,6 +105,8 @@ object build extends Build {
       if (index.exists()) Desktop.getDesktop.open(out / "index.html")
     },
 
+    credentialsSetting,
+    publishSetting,
     publishArtifact in Test := false,
 
     // adapted from sbt-release defaults
