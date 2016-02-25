b91c2048a797e76a36fce9bab296bf5f3f6d6b1d
David Barri <japgolly@gmail.com>
2016-02-25 18:39:58 +1100

    phantom

--- project/build.scala
+++ project/build.scala
@@ -284,7 +284,7 @@ object build extends Build {
       libraryDependencies += "org.scalacheck" %%% "scalacheck" % scalaCheckVersion.value % "test")
     .dependsOn(core, effect, iteratee, scalacheckBinding)
     .jvmConfigure(_ dependsOn concurrent)
-    // .jsSettings(jsEnv := PhantomJSEnv().value) // Faster
+    .jsSettings(jsEnv := PhantomJSEnv().value) // Faster
 
   lazy val testsJVM = tests.jvm
   lazy val testsJS  = tests.js
